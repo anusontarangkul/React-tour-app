2bec6bfccc4702b6431f34e9eff3bfc5a7f35cd5
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-03-31 23:48:43 -0700

    add setTours state

--- src/App.js
+++ src/App.js
@@ -9,9 +9,17 @@ function App() {
 
     const fetchTours = async () => {
         setLoading(true);
-        const response = await fetch(url);
-        const tours = await response.json();
-        console.log(tours)
+        try {
+            const response = await fetch(url);
+            const tours = await response.json();
+            setLoading(false);
+            setTours(tours)
+        } catch (error) {
+            setLoading(false)
+            console.log(error)
+        }
+
+
     };
     useEffect(() => {
         fetchTours();
